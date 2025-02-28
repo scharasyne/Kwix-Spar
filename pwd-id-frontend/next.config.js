bd16f52f690331a5c94751cb44efe295e98df5dd
scharasyne <118493742+scharasyne@users.noreply.github.com>
2025-02-28 10:52:09 +0800

    Delete pwd-id-frontend directory

--- pwd-id-frontend/next.config.js
+++
@@ -1,8 +0,0 @@
-// filepath: /c:/Users/Glenn Solis/Projects/Hackathon1/pwd-id-frontend/next.config.js
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-};
-
-module.exports = nextConfig;
